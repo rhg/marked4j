41d0c9ff6c3f9688eca60662c26b3704fb9a06e4
Ricardo Gomez <ricardo.gomez331@gmail.com>
2015-07-23 21:08:11 -0500

    set scm metadata for the marked4j lib

--- marked4j/project.clj
+++ marked4j/project.clj
@@ -3,5 +3,8 @@
   :url "https://github.com/rhg/marked4j"
   :license {:name "CC0"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :scm {:name "git"
+        :url "https://github.com/rhg/marked4j"
+        :dir ".."}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.rhg135.marked4j/marked "0.3.2"]])
