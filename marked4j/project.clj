943b684d9b7dc9f41da82014fc11c621a860906b
Ricardo Gomez <ricardo.gomez331@gmail.com>
2015-07-25 15:03:02 -0500

    alpha1

--- marked4j/project.clj
+++ marked4j/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rhg135/marked4j "0.0.3-SNAPSHOT"
+(defproject org.clojars.rhg135/marked4j "0.0.3-alpha1"
   :description "Small library for turning markdown to html using marked"
   :url "https://github.com/rhg/marked4j"
   :license {:name "CC0"
