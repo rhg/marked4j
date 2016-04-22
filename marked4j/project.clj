31c634b1d4040cb393e57352c694d7a41f431e94
Ricardo Gomez <ricardo.gomez331@gmail.com>
2016-04-21 23:36:02 -0500

    Add a small cli
    we require our ns at runtime to speed up invalid input or help

--- marked4j/project.clj
+++ marked4j/project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/rhg/marked4j"
   :license {:name "CC0"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :main com.rhg135.marked4j.main
   :scm {:name "git"
         :url "https://github.com/rhg/marked4j"
         :dir ".."}
---
+++ marked4j/src/com/rhg135/marked4j/main.clj
@@ -0,0 +1,21 @@
+(ns com.rhg135.marked4j.main
+  (:gen-class))
+
+(defn do-main
+  []
+  (let [in (slurp *in*)
+        _ (require 'com.rhg135.marked4j)
+        out ((resolve 'com.rhg135.marked4j/marked) in)]
+    (print out)))
+
+(defn do-usage
+  []
+  (binding [*out* *err*]
+    (println "USAGE: java -jar marked4j.jar"))
+  (System/exit 2))
+
+(defn -main ; TODO: be able to pass options
+  [& args]
+  (if (empty? args)
+    (do-main)
+    (do-usage)))
